b15898355f33af3edc846504aeb2e53d31df2a2d
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-17 23:18:21 +0530

    firebase connected

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase ,ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://addtocart-db-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListDb =ref(database,"shoppingList")
+
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
+
+ push(shoppingListDb,inputValue);
 
   console.log(inputValue);
 });
