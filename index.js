60f5f57767ae745c983421965cce611edbe0862d
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-17 23:24:34 +0530

    dynamically rendered lists

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const shoppingListDb =ref(database,"shoppingList")
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
   let inputValue = inputFieldEl.value;
 
  push(shoppingListDb,inputValue);
+inputFieldEl.value = "";
 
+shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
   console.log(inputValue);
 });
